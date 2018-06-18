daa2f735c8171b91324cc2a3c99f2062a4bded85
Taha Bal <taha-bal@hotmail.com>
2018-06-18 21:26:20 +0300

    "added react-snapshot for static hosting"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
